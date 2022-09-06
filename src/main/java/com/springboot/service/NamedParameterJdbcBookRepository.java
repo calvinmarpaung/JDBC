a605dc349cc17bc28543014bd65e044d7a3cbad5
Calvin Haposan Marpaung <106538712+calvinmarpaung@users.noreply.github.com>
2022-09-06 16:49:29 +0700

    Update NamedParameterJdbcBookRepository.java

--- src/main/java/com/springboot/service/NamedParameterJdbcBookRepository.java
+++ src/main/java/com/springboot/service/NamedParameterJdbcBookRepository.java
@@ -9,7 +9,6 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 
